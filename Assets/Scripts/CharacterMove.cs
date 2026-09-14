c4dacb74f1bdf10f6bcb518dfa4449eac517b125
juliagnaty <julia28052010j@gmail.com>
2026-09-14 08:36:02 -0300

    Movimento character controller
    Adição do código de movimento utilizando o character controller (sem pulo) e materiais e objetos para teste

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour
 {
+    private CharacterController controlador;
+    public float velocidade = 6f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        controlador = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 direcao = new Vector3(x, 0, z);
+        controlador.SimpleMove(direcao * velocidade);
     }
 }
